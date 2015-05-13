0480a82f3ac30aede3a8e97f1305c93a1deb9a33
William Towe <willbur1984@gmail.com>
2015-05-13 13:36:24 -0400

    Initial Commit

---
+++ BBFrameworks/BBFrameworks.h
@@ -0,0 +1,19 @@
+//
+//  BBFrameworks.h
+//  BBFrameworks
+//
+//  Created by William Towe on 5/13/15.
+//  Copyright (c) 2015 Bion Bilateral, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BBFrameworks.
+FOUNDATION_EXPORT double BBFrameworksVersionNumber;
+
+//! Project version string for BBFrameworks.
+FOUNDATION_EXPORT const unsigned char BBFrameworksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBFrameworks/PublicHeader.h>
+
+
