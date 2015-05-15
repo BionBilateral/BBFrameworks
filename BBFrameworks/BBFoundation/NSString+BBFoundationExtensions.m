f2ee1374947a082b5f40975483fecfccb756e73d
William Towe <willbur1984@gmail.com>
2015-05-15 19:45:45 -0400

    added NSString+BBFoundationExtensions.h/m

---
+++ BBFrameworks/BBFoundation/NSString+BBFoundationExtensions.m
@@ -0,0 +1,34 @@
+//
+//  NSString+BBFoundationExtensions.m
+//  BBFrameworks
+//
+//  Created by William Towe on 5/15/15.
+//  Copyright (c) 2015 Bion Bilateral, LLC. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#import "NSString+BBFoundationExtensions.h"
+#import "NSData+BBFoundationExtensions.h"
+
+@implementation NSString (BBFoundationExtensions)
+
+- (NSString *)BB_MD5String; {
+    return [[self dataUsingEncoding:NSUTF8StringEncoding] BB_MD5String];
+}
+- (NSString *)BB_SHA1String; {
+    return [[self dataUsingEncoding:NSUTF8StringEncoding] BB_SHA1String];
+}
+- (NSString *)BB_SHA256String; {
+    return [[self dataUsingEncoding:NSUTF8StringEncoding] BB_SHA256String];
+}
+- (NSString *)BB_SHA512String; {
+    return [[self dataUsingEncoding:NSUTF8StringEncoding] BB_SHA512String];
+}
+
+@end
