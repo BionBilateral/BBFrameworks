9872763d182b471eb8a9985ed2077ef81c38e07e
William Towe <willbur1984@gmail.com>
2015-06-09 19:37:33 -0400

    updated BBFoundation.h

--- BBFrameworks/BBKit/UIFont+BBKitExtensions.m
+++ BBFrameworks/BBKit/UIFont+BBKitExtensions.m
@@ -1,8 +1,8 @@
 //
-//  BBFoundation.h
+//  UIFont+BBKitExtensions.m
 //  BBFrameworks
 //
-//  Created by William Towe on 5/13/15.
+//  Created by William Towe on 6/8/15.
 //  Copyright (c) 2015 Bion Bilateral, LLC. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -13,21 +13,17 @@
 //
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef __BB_FRAMEWORKS_FOUNDATION__
-#define __BB_FRAMEWORKS_FOUNDATION__
+#import "UIFont+BBKitExtensions.h"
 
-#import <BBFrameworks/BBFoundationDebugging.h>
-#import <BBFrameworks/BBFoundationMacros.h>
-#import <BBFrameworks/BBFoundationFunctions.h>
+@implementation UIFont (BBKitExtensions)
 
-#import <BBFrameworks/NSFileManager+BBFoundationExtensions.h>
-#import <BBFrameworks/NSArray+BBFoundationExtensions.h>
-#import <BBFrameworks/NSMutableArray+BBFoundationExtensions.h>
-#import <BBFrameworks/NSData+BBFoundationExtensions.h>
-#import <BBFrameworks/NSString+BBFoundationExtensions.h>
-#import <BBFrameworks/NSBundle+BBFoundationExtensions.h>
-#import <BBFrameworks/NSURL+BBFoundationExtensions.h>
++ (UIFont *)BB_fontWithName:(NSString *)fontName textStyle:(NSString *)textStyle; {
+    NSParameterAssert(fontName);
+    NSParameterAssert(textStyle);
+    
+    UIFontDescriptor *fontDesc = [UIFontDescriptor preferredFontDescriptorWithTextStyle:textStyle];
+    
+    return [UIFont fontWithName:fontName size:fontDesc.pointSize];
+}
 
-#import <BBFrameworks/BBSnakeCaseToLlamaCaseValueTransformer.h>
-
-#endif
+@end
