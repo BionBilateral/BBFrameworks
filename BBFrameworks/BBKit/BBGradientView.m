90de8ba9ee86f4df9a6225756835e233dc7f62ba
Jason Anderson <jranderson@kosoku.com>
2015-06-24 17:02:49 -0400

    finish out iOS/OS X implementation of BBGradientView
    add setters/getters for locations, start/end points, and finished OS X color array return value. Also, updated header comment to reflect locations array as an array of NSNumbers

--- BBFrameworks/BBKit/BBGradientView.m
+++ BBFrameworks/BBKit/BBGradientView.m
@@ -14,6 +14,7 @@
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import "BBGradientView.h"
+#import <QuartzCore/CAGradientLayer.h>
 
 @interface BBGradientView ()
 @property (readonly,nonatomic) CAGradientLayer *gradientLayer;
@@ -53,12 +54,13 @@
 #if (TARGET_OS_IPHONE)
         [retval addObject:[UIColor colorWithCGColor:colorRef]];
 #else
-        
+        [retval addObject:[NSColor colorWithCGColor:colorRef]];
 #endif
     }
     
     return [retval copy];
 }
+    
 - (void)setColors:(NSArray *)colors {
     NSMutableArray *temp = [[NSMutableArray alloc] init];
     
@@ -72,8 +74,55 @@
     
     [self.gradientLayer setColors:temp];
 }
+    
+@dynamic locations;
+- (NSArray *)locations {
+    return self.gradientLayer.locations;
+}
+    
+- (void)setLocations:(NSArray *)locations {
+    [self.gradientLayer setLocations:locations];
+}
 
-- (void)_BBGradientViewInit; {
+@dynamic startPoint;
+#if (TARGET_OS_IPHONE)
+- (CGPoint)startPoint {
+    return self.gradientLayer.startPoint;
+#else
+- (NSPoint)startPoint {
+    return NSPointFromCGPoint(self.gradientLayer.startPoint);
+#endif
+}
+
+#if (TARGET_OS_IPHONE)
+- (void)setStartPoint:(CGPoint)startPoint {
+    [self.gradientLayer setStartPoint:startPoint];
+#else
+- (void)setStartPoint:(NSPoint)startPoint {
+    [self.gradientLayer setStartPoint:NSPointToCGPoint(startPoint)];
+#endif
+}
+
+@dynamic endPoint;
+#if (TARGET_OS_IPHONE)
+- (CGPoint)endPoint {
+    return self.gradientLayer.endPoint;
+#else
+- (NSPoint)endPoint {
+    return NSPointFromCGPoint(self.gradientLayer.endPoint);
+#endif
+}
+
+#if (TARGET_OS_IPHONE)
+- (void)setEndPoint:(CGPoint)endPoint {
+    [self.gradientLayer setEndPoint:endPoint];
+#else
+- (void)setEndPoint:(NSPoint)endPoint {
+    [self.gradientLayer setEndPoint:NSPointToCGPoint(endPoint)];
+#endif
+}
+
+- (void)_BBGradientViewInit {
 #if (TARGET_OS_IPHONE)
     [self setBackgroundColor:[UIColor clearColor]];
 #else
