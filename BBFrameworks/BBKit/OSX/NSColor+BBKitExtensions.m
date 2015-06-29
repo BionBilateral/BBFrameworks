4d0ca37a29e2f18cf8a971daf982546a05224d71
William Towe <willbur1984@gmail.com>
2015-06-29 13:40:16 -0400

    fix BB_colorWithHexadecimalString: on osx

--- BBFrameworks/BBKit/OSX/NSColor+BBKitExtensions.m
+++ BBFrameworks/BBKit/OSX/NSColor+BBKitExtensions.m
@@ -45,13 +45,13 @@
     NSColor *retval = nil;
     NSScanner *scanner = [NSScanner scannerWithString:hexadecimalString];
     
-    unsigned int hexadecimalColor;
+    uint32_t hexadecimalColor;
     if (![scanner scanHexInt:&hexadecimalColor]) {
         return retval;
     }
     
-    uint8_t red = (uint8_t)hexadecimalColor << 16;
-    uint8_t green = (uint8_t)hexadecimalColor << 8;
+    uint8_t red = (uint8_t)(hexadecimalColor >> 16);
+    uint8_t green = (uint8_t)(hexadecimalColor >> 8);
     uint8_t blue = (uint8_t)hexadecimalColor;
     
     retval = [NSColor colorWithCalibratedRed:(CGFloat)red/0xff green:(CGFloat)green/0xff blue:(CGFloat)blue/0xff alpha:1.0];
