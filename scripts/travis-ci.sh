db025642a6894e7047e1e89a369d7ebeecbc613d
William Towe <willbur1984@gmail.com>
2015-07-02 12:33:28 -0400

    build tests in travis-ci using latest iphonesimulator

--- scripts/travis-ci.sh
+++ scripts/travis-ci.sh
@@ -1,2 +1,2 @@
-xcodebuild -workspace BBFrameworks.xcworkspace -scheme BBFrameworksTestsiOS test
-xcodebuild -workspace BBFrameworks.xcworkspace -scheme BBFrameworksTestsOSX test
+xcodebuild -workspace BBFrameworks.xcworkspace -scheme BBFrameworksTestsiOS -sdk iphonesimulator test
+xcodebuild -workspace BBFrameworks.xcworkspace -scheme BBFrameworksTestsOSX -sdk iphonesimulator test
