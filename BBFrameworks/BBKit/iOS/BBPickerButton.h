1960c8ba181a2d956e6e0419fc47b971516170c3
William Towe <willbur1984@gmail.com>
2015-07-10 16:48:49 -0400

    begin to stub out BBPickerButton

--- BBFrameworks/BBKit/iOS/BBPickerButton.h
+++ BBFrameworks/BBKit/iOS/BBPickerButton.h
@@ -1,8 +1,8 @@
 //
-//  BBKit.h
+//  BBPickerButton.h
 //  BBFrameworks
 //
-//  Created by William Towe on 5/13/15.
+//  Created by William Towe on 7/10/15.
 //  Copyright (c) 2015 Bion Bilateral, LLC. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -13,27 +13,13 @@
 //
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef __BB_FRAMEWORKS_KIT__
-#define __BB_FRAMEWORKS_KIT__
+#import <UIKit/UIKit.h>
+#import "BBPickerButtonDataSource.h"
 
-#import <BBFrameworks/BBKitColorMacros.h>
+@interface BBPickerButton : UIButton
 
-#import <BBFrameworks/NSURL+BBKitExtensions.h>
-#if (TARGET_OS_IPHONE)
-#import <BBFrameworks/UIImage+BBKitExtensions.h>
-#import <BBFrameworks/UIView+BBKitExtensions.h>
-#import <BBFrameworks/UIViewController+BBKitExtensions.h>
-#import <BBFrameworks/UIFont+BBKitExtensions.h>
-#import <BBFrameworks/UIBarButtonItem+BBKitExtensions.h>
-#import <BBFrameworks/UIAlertController+BBKitExtensions.h>
-#else
-#import <BBFrameworks/NSImage+BBKitExtensions.h>
-#import <BBFrameworks/NSAlert+BBKitExtensions.h>
-#endif
+@property (weak,nonatomic) id<BBPickerButtonDataSource> dataSource;
 
-#import <BBFrameworks/BBBadgeView.h>
-#import <BBFrameworks/BBGradientView.h>
-#import <BBFrameworks/BBView.h>
-#import <BBFrameworks/BBTextField.h>
-#import <BBFrameworks/BBPickerButton.h>
-#endif
+- (void)reloadData;
+
+@end
--- BBFrameworks/BBKit/iOS/BBPickerButton.m
+++ BBFrameworks/BBKit/iOS/BBPickerButton.m
@@ -1,8 +1,8 @@
 //
-//  BBKit.h
+//  BBPickerButton.m
 //  BBFrameworks
 //
-//  Created by William Towe on 5/13/15.
+//  Created by William Towe on 7/10/15.
 //  Copyright (c) 2015 Bion Bilateral, LLC. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -13,27 +13,50 @@
 //
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef __BB_FRAMEWORKS_KIT__
-#define __BB_FRAMEWORKS_KIT__
+#import "BBPickerButton.h"
 
-#import <BBFrameworks/BBKitColorMacros.h>
+@interface BBPickerButton () <UIPickerViewDataSource,UIPickerViewDelegate>
+@property (strong,nonatomic) UIPickerView *pickerView;
 
-#import <BBFrameworks/NSURL+BBKitExtensions.h>
-#if (TARGET_OS_IPHONE)
-#import <BBFrameworks/UIImage+BBKitExtensions.h>
-#import <BBFrameworks/UIView+BBKitExtensions.h>
-#import <BBFrameworks/UIViewController+BBKitExtensions.h>
-#import <BBFrameworks/UIFont+BBKitExtensions.h>
-#import <BBFrameworks/UIBarButtonItem+BBKitExtensions.h>
-#import <BBFrameworks/UIAlertController+BBKitExtensions.h>
-#else
-#import <BBFrameworks/NSImage+BBKitExtensions.h>
-#import <BBFrameworks/NSAlert+BBKitExtensions.h>
-#endif
+- (void)_BBPickerButtonInit;
+@end
 
-#import <BBFrameworks/BBBadgeView.h>
-#import <BBFrameworks/BBGradientView.h>
-#import <BBFrameworks/BBView.h>
-#import <BBFrameworks/BBTextField.h>
-#import <BBFrameworks/BBPickerButton.h>
-#endif
+@implementation BBPickerButton
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    if (!(self = [super initWithFrame:frame]))
+        return nil;
+    
+    [self _BBPickerButtonInit];
+    
+    return self;
+}
+- (id)initWithCoder:(NSCoder *)aDecoder {
+    if (!(self = [super initWithCoder:aDecoder]))
+        return nil;
+    
+    [self _BBPickerButtonInit];
+    
+    return self;
+}
+
+- (BOOL)canBecomeFirstResponder {
+    return YES;
+}
+- (BOOL)becomeFirstResponder {
+    return YES;
+}
+- (BOOL)resignFirstResponder {
+    return YES;
+}
+
+- (void)reloadData; {
+    [self.pickerView reloadAllComponents];
+}
+
+- (void)_BBPickerButtonInit; {
+    [self setPickerView:[[UIPickerView alloc] initWithFrame:CGRectZero]];
+    [self.pickerView sizeToFit];
+}
+
+@end
