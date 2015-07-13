56cde6feafd307611f2b536216ed820bc5c9d4b9
William Towe <willbur1984@gmail.com>
2015-07-13 13:18:38 -0400

    more refactoring around completions

--- BBFrameworks/BBToken/BBTokenTextView.m
+++ BBFrameworks/BBToken/BBTokenTextView.m
@@ -69,6 +69,10 @@
 - (void)_BBTokenFieldInit;
 
 - (void)_showCompletionsTableView;
+- (void)_hideCompletionsTableViewAndSelectCompletion:(id<BBTokenCompletion>)completion;
+
+- (NSRange)_completionRangeForRange:(NSRange)range;
+- (BBTokenTextAttachment *)_tokenTextAttachmentForRange:(NSRange)range index:(NSInteger *)index;
 
 + (NSCharacterSet *)_defaultTokenizingCharacterSet;
 + (NSTimeInterval)_defaultCompletionDelay;
@@ -122,16 +126,7 @@
     if ([text rangeOfCharacterFromSet:self.tokenizingCharacterSet].length > 0 ||
         [text rangeOfCharacterFromSet:[NSCharacterSet newlineCharacterSet]].length > 0) {
         
-        NSRange searchRange = NSMakeRange(0, range.location);
-        NSRange foundRange = [self.text rangeOfCharacterFromSet:[NSCharacterSet alphanumericCharacterSet] options:NSBackwardsSearch range:searchRange];
-        NSRange tokenRange = foundRange;
-        
-        while (foundRange.length > 0) {
-            tokenRange = NSUnionRange(tokenRange, foundRange);
-            
-            searchRange = NSMakeRange(0, foundRange.location);
-            foundRange = [self.text rangeOfCharacterFromSet:[NSCharacterSet alphanumericCharacterSet] options:NSBackwardsSearch range:searchRange];
-        }
+        NSRange tokenRange = [self _completionRangeForRange:range];
         
         if (tokenRange.length > 0) {
             NSString *tokenText = [[self.text substringWithRange:tokenRange] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
@@ -142,18 +137,14 @@
             }
             
             NSArray *representedObjects = @[representedObject];
-            BBTokenTextAttachment *textAttachment = [self.attributedText attribute:NSAttachmentAttributeName atIndex:MIN(range.location, self.attributedText.length - 1) effectiveRange:NULL];
-            NSInteger index = [self.representedObjects indexOfObject:textAttachment.representedObject];
-            
-            if (index == NSNotFound) {
-                index = self.representedObjects.count;
-            }
+            NSInteger index;
+            [self _tokenTextAttachmentForRange:range index:&index];
             
             if ([self.delegate respondsToSelector:@selector(tokenTextView:shouldAddRepresentedObjects:atIndex:)]) {
                 representedObjects = [self.delegate tokenTextView:self shouldAddRepresentedObjects:representedObjects atIndex:index];
             }
             
-            if (representedObjects) {
+            if (representedObjects.count > 0) {
                 NSMutableAttributedString *temp = [[NSMutableAttributedString alloc] initWithString:@"" attributes:@{NSFontAttributeName: self.typingFont, NSForegroundColorAttributeName: self.typingTextColor}];
                 
                 for (id obj in representedObjects) {
@@ -169,6 +160,8 @@
                 [self.textStorage replaceCharactersInRange:tokenRange withAttributedString:temp];
                 
                 [self setSelectedRange:NSMakeRange(tokenRange.location + 1, 0)];
+                
+                [self _hideCompletionsTableViewAndSelectCompletion:nil];
             }
         }
         
@@ -187,12 +180,8 @@
             
             if (representedObjects.count > 0) {
                 if ([self.delegate respondsToSelector:@selector(tokenTextView:didRemoveRepresentedObjects:atIndex:)]) {
-                    BBTokenTextAttachment *textAttachment = self.text.length == 0 ? nil : [self.attributedText attribute:NSAttachmentAttributeName atIndex:MIN(range.location, self.attributedText.length - 1) effectiveRange:NULL];
-                    NSInteger index = [self.representedObjects indexOfObject:textAttachment.representedObject];
-                    
-                    if (index == NSNotFound) {
-                        index = 0;
-                    }
+                    NSInteger index;
+                    [self _tokenTextAttachmentForRange:range index:&index];
                     
                     [self.delegate tokenTextView:self didRemoveRepresentedObjects:representedObjects atIndex:index];
                 }
@@ -226,19 +215,7 @@
 }
 #pragma mark UITableViewDelegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
-    if ([self.delegate respondsToSelector:@selector(tokenTextView:hideCompletionsTableView:)]) {
-        id<BBTokenCompletion> completion = self.completions[indexPath.row];
-        id representedObject = [self.delegate tokenTextView:self representedObjectForEditingText:[completion tokenCompletionTitle]];
-        NSString *text = [self.delegate tokenTextView:self displayTextForRepresentedObject:representedObject];
-        NSTextAttachment *attachment = [[self.tokenTextAttachmentClass alloc] initWithRepresentedObject:representedObject text:text tokenTextView:self];
-        NSTextCheckingResult *result = [[NSRegularExpression regularExpressionWithPattern:@"[A-Aa-z0-9_-]+" options:0 error:NULL] firstMatchInString:self.text options:0 range:NSMakeRange(0, self.text.length)];
-        
-        [self.textStorage replaceCharactersInRange:result.range withAttributedString:[NSAttributedString attributedStringWithAttachment:attachment]];
-        
-        [self.delegate tokenTextView:self hideCompletionsTableView:self.tableView];
-        
-        [self setTableView:nil];
-    }
+    [self _hideCompletionsTableViewAndSelectCompletion:self.completions[indexPath.row]];
 }
 #pragma mark *** Public Methods ***
 #pragma mark Properties
@@ -310,6 +287,11 @@
 }
 
 - (void)_showCompletionsTableView; {
+    if ([self _completionRangeForRange:self.selectedRange].length == 0) {
+        [self _hideCompletionsTableViewAndSelectCompletion:nil];
+        return;
+    }
+    
     if (!self.tableView) {
         if ([self.delegate respondsToSelector:@selector(tokenTextView:showCompletionsTableView:)]) {
             [self setTableView:[[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain]];
@@ -321,30 +303,81 @@
         }
     }
     
-    if ([self.delegate respondsToSelector:@selector(tokenTextView:completionsForSubstring:indexOfRepresentedObject:completion:)]) {
-        NSTextCheckingResult *result = [[NSRegularExpression regularExpressionWithPattern:@"[A-Aa-z0-9_-]+" options:0 error:NULL] firstMatchInString:self.text options:0 range:NSMakeRange(0, self.text.length)];
-        BBTokenTextAttachment *textAttachment = self.text.length == 0 ? nil : [self.attributedText attribute:NSAttachmentAttributeName atIndex:MIN(self.selectedRange.location, self.attributedText.length - 1) effectiveRange:NULL];
-        NSInteger index = [self.representedObjects indexOfObject:textAttachment.representedObject];
+    if ([self.delegate respondsToSelector:@selector(tokenTextView:completionsForSubstring:indexOfRepresentedObject:completion:)] ||
+        [self.delegate respondsToSelector:@selector(tokenTextView:completionsForSubstring:indexOfRepresentedObject:)]) {
         
-        if (index == NSNotFound) {
-            index = self.representedObjects.count;
+        NSInteger index;
+        [self _tokenTextAttachmentForRange:self.selectedRange index:&index];
+        
+        NSRange range = [self _completionRangeForRange:self.selectedRange];
+        
+        if ([self.delegate respondsToSelector:@selector(tokenTextView:completionsForSubstring:indexOfRepresentedObject:completion:)]) {
+            [self.delegate tokenTextView:self completionsForSubstring:[self.text substringWithRange:range] indexOfRepresentedObject:index completion:^(NSArray *completions) {
+                [self setCompletions:completions];
+            }];
+        }
+        else {
+            [self setCompletions:[self.delegate tokenTextView:self completionsForSubstring:[self.text substringWithRange:range] indexOfRepresentedObject:index]];
+        }
+    }
+}
+- (void)_hideCompletionsTableViewAndSelectCompletion:(id<BBTokenCompletion>)completion; {
+    if ([self.delegate respondsToSelector:@selector(tokenTextView:hideCompletionsTableView:)]) {
+        if (completion) {
+            id representedObject = [self.delegate tokenTextView:self representedObjectForEditingText:[completion tokenCompletionTitle]];
+            NSString *text = [self.delegate tokenTextView:self displayTextForRepresentedObject:representedObject];
+            NSTextAttachment *attachment = [[self.tokenTextAttachmentClass alloc] initWithRepresentedObject:representedObject text:text tokenTextView:self];
+            
+            [self.textStorage replaceCharactersInRange:[self _completionRangeForRange:self.selectedRange] withAttributedString:[NSAttributedString attributedStringWithAttachment:attachment]];
         }
         
-        [self.delegate tokenTextView:self completionsForSubstring:[self.text substringWithRange:result.range] indexOfRepresentedObject:index completion:^(NSArray *completions) {
-            [self setCompletions:completions];
-        }];
-    }
-    else if ([self.delegate respondsToSelector:@selector(tokenTextView:completionsForSubstring:indexOfRepresentedObject:)]) {
-        NSTextCheckingResult *result = [[NSRegularExpression regularExpressionWithPattern:@"[A-Aa-z0-9_-]+" options:0 error:NULL] firstMatchInString:self.text options:0 range:NSMakeRange(0, self.text.length)];
-        BBTokenTextAttachment *textAttachment = self.text.length == 0 ? nil : [self.attributedText attribute:NSAttachmentAttributeName atIndex:MIN(self.selectedRange.location, self.attributedText.length - 1) effectiveRange:NULL];
-        NSInteger index = [self.representedObjects indexOfObject:textAttachment.representedObject];
+        [self.delegate tokenTextView:self hideCompletionsTableView:self.tableView];
         
-        if (index == NSNotFound) {
-            index = self.representedObjects.count;
+        [self setTableView:nil];
+    }
+}
+
+- (NSRange)_completionRangeForRange:(NSRange)range; {
+    NSRange searchRange = NSMakeRange(0, range.location);
+    NSCharacterSet *characterSet = self.tokenizingCharacterSet.invertedSet;
+    NSRange foundRange = [self.text rangeOfCharacterFromSet:characterSet options:NSBackwardsSearch range:searchRange];
+    NSRange retval = foundRange;
+    
+    // first search backwards
+    while (foundRange.length > 0) {
+        retval = NSUnionRange(retval, foundRange);
+        
+        searchRange = NSMakeRange(0, foundRange.location);
+        foundRange = [self.text rangeOfCharacterFromSet:characterSet options:NSBackwardsSearch range:searchRange];
+    }
+    
+    // search forwards
+    searchRange = NSMakeRange(range.location, self.text.length - range.location);
+    foundRange = [self.text rangeOfCharacterFromSet:characterSet options:0 range:searchRange];
+    
+    while (foundRange.length > 0) {
+        retval = NSUnionRange(retval, foundRange);
+        
+        searchRange = NSMakeRange(NSMaxRange(foundRange), self.text.length - NSMaxRange(foundRange));
+        foundRange = [self.text rangeOfCharacterFromSet:characterSet options:0 range:searchRange];
+    }
+    
+    return retval;
+}
+- (BBTokenTextAttachment *)_tokenTextAttachmentForRange:(NSRange)range index:(NSInteger *)index; {
+    BBTokenTextAttachment *retval = self.text.length == 0 ? nil : [self.attributedText attribute:NSAttachmentAttributeName atIndex:MIN(range.location, self.attributedText.length - 1) effectiveRange:NULL];
+    
+    if (index) {
+        NSInteger outIndex = [self.representedObjects indexOfObject:retval.representedObject];
+        
+        if (outIndex == NSNotFound) {
+            outIndex = self.representedObjects.count;
         }
         
-        [self setCompletions:[self.delegate tokenTextView:self completionsForSubstring:[self.text substringWithRange:result.range] indexOfRepresentedObject:index]];
+        *index = outIndex;
     }
+    
+    return retval;
 }
 
 + (NSCharacterSet *)_defaultTokenizingCharacterSet; {
