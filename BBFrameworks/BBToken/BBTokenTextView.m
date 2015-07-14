93952d9d149bc31c03c6299b4cf63de457fa2a81
William Towe <willbur1984@gmail.com>
2015-07-14 13:28:15 -0400

    added clarifying comments in the BBTokenTextView.m

--- BBFrameworks/BBToken/BBTokenTextView.m
+++ BBFrameworks/BBToken/BBTokenTextView.m
@@ -19,19 +19,27 @@
 
 #import <MobileCoreServices/MobileCoreServices.h>
 
+/**
+ _BBTokenTextViewInternalDelegate is a NSObject subclass that serves as our actual delegate and allows forwarding messages to us, as well as passing through all delegate messages to an external delegate.
+ */
 @interface _BBTokenTextViewInternalDelegate : NSObject <BBTokenTextViewDelegate>
+/**
+ Set and get the external delegate.
+ */
 @property (weak,nonatomic) id<BBTokenTextViewDelegate> delegate;
 @end
 
 @implementation _BBTokenTextViewInternalDelegate
-
+// Does the external delegate respond to aSelector or does our super class?
 - (BOOL)respondsToSelector:(SEL)aSelector {
     return [self.delegate respondsToSelector:aSelector] || [super respondsToSelector:aSelector];
 }
+// forwardInvocation is only called if the receiver does not respond to a selector but respondsToSelector: returned YES. This will only happen if the external delegate responds to a delegate method that we do not implement.
 - (void)forwardInvocation:(NSInvocation *)anInvocation {
     [anInvocation invokeWithTarget:self.delegate];
 }
 
+// Pass through delegate messages we are interested to the owning text view, as well as our external delegate.
 - (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
     BOOL retval = [(id<UITextViewDelegate>)textView textView:textView shouldChangeTextInRange:range replacementText:text];
     
@@ -114,7 +122,7 @@ static void *kObservingContext = &kObservingContext;
     
     return self;
 }
-
+// disable cut:, copy:, and paste: for now, need to investigate how to duplicate Mail.app interactions using text attachments
 - (BOOL)canPerformAction:(SEL)action withSender:(id)sender {
     if (action == @selector(cut:) ||
         action == @selector(copy:) ||
@@ -126,6 +134,7 @@ static void *kObservingContext = &kObservingContext;
 }
 
 @dynamic delegate;
+// the internal delegate tracks the external delegate that is set on the receiver
 - (void)setDelegate:(id<BBTokenTextViewDelegate>)delegate {
     [self.internalDelegate setDelegate:delegate];
     
@@ -134,6 +143,7 @@ static void *kObservingContext = &kObservingContext;
 #pragma mark NSKeyValueObserving
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
     if (context == kObservingContext) {
+        // if typing font or text color change, update our typing attributes appropriately
         if ([keyPath isEqualToString:kTypingFontKey] ||
             [keyPath isEqualToString:kTypingTextColorKey]) {
             
@@ -146,32 +156,40 @@ static void *kObservingContext = &kObservingContext;
 }
 #pragma mark UITextViewDelegate
 - (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text {
-    // tokenize character set
-    // return
+    // the user either typed a newline character or a character from the tokenizing character set
     if ([text rangeOfCharacterFromSet:self.tokenizingCharacterSet].length > 0 ||
         [text rangeOfCharacterFromSet:[NSCharacterSet newlineCharacterSet]].length > 0) {
         
         NSRange tokenRange = [self _completionRangeForRange:range];
         
+        // if we have a non-zero length token range, continue
         if (tokenRange.length > 0) {
+            // trim surrounding whitespace to prevent something like " a@b.com" being shown as a token
             NSString *tokenText = [[self.text substringWithRange:tokenRange] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
+            // initially the represented object is the token text itself
             id representedObject = tokenText;
             
+            // if the delegate implements tokenTextView:representedObjectForEditingText: use its return value for the represented object
             if ([self.delegate respondsToSelector:@selector(tokenTextView:representedObjectForEditingText:)]) {
                 representedObject = [self.delegate tokenTextView:self representedObjectForEditingText:tokenText];
             }
             
+            // initial array of represented objects to insert
             NSArray *representedObjects = @[representedObject];
+            // index to insert the objects at
             NSInteger index;
             [self _tokenTextAttachmentForRange:range index:&index];
             
+            // if the delegate responds to tokenTextView:shouldAddRepresentedObjects:atIndex use its return value for the represented objects to insert
             if ([self.delegate respondsToSelector:@selector(tokenTextView:shouldAddRepresentedObjects:atIndex:)]) {
                 representedObjects = [self.delegate tokenTextView:self shouldAddRepresentedObjects:representedObjects atIndex:index];
             }
             
+            // if there are represented objects to insert, continue
             if (representedObjects.count > 0) {
                 NSMutableAttributedString *temp = [[NSMutableAttributedString alloc] initWithString:@"" attributes:@{NSFontAttributeName: self.typingFont, NSForegroundColorAttributeName: self.typingTextColor}];
                 
+                // loop through each represented object and ask the delegate for the display text for each one
                 for (id obj in representedObjects) {
                     NSString *displayText = [obj description];
                     
@@ -181,11 +199,13 @@ static void *kObservingContext = &kObservingContext;
                     
                     [temp appendAttributedString:[NSAttributedString attributedStringWithAttachment:[[self.tokenTextAttachmentClass alloc] initWithRepresentedObject:obj text:displayText tokenTextView:self]]];
                 }
-
+                
+                // replace all characters in token range with the text attachments
                 [self.textStorage replaceCharactersInRange:tokenRange withAttributedString:temp];
                 
                 [self setSelectedRange:NSMakeRange(tokenRange.location + 1, 0)];
                 
+                // hide the completion table view if it was visible
                 [self _hideCompletionsTableViewAndSelectCompletion:nil];
             }
         }
@@ -197,12 +217,14 @@ static void *kObservingContext = &kObservingContext;
         if (self.text.length > 0) {
             NSMutableArray *representedObjects = [[NSMutableArray alloc] init];
             
+            // enumerate text attachments in the range to be deleted and add their represented object to the array
             [self.textStorage enumerateAttribute:NSAttachmentAttributeName inRange:range options:NSAttributedStringEnumerationLongestEffectiveRangeNotRequired usingBlock:^(BBTokenDefaultTextAttachment *value, NSRange range, BOOL *stop) {
                 if (value) {
                     [representedObjects addObject:value.representedObject];
                 }
             }];
             
+            // if there are text attachments, call tokenTextView:didRemoveRepresentedObjects:atIndex: if its implemented
             if (representedObjects.count > 0) {
                 if ([self.delegate respondsToSelector:@selector(tokenTextView:didRemoveRepresentedObjects:atIndex:)]) {
                     NSInteger index;
@@ -255,6 +277,7 @@ static void *kObservingContext = &kObservingContext;
 }
 #pragma mark UITableViewDelegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    // hide the completions table view and insert the selected completion
     [self _hideCompletionsTableViewAndSelectCompletion:self.completions[indexPath.row]];
 }
 #pragma mark *** Public Methods ***
@@ -274,7 +297,7 @@ static void *kObservingContext = &kObservingContext;
 - (void)setRepresentedObjects:(NSArray *)representedObjects {
     NSMutableAttributedString *temp = [[NSMutableAttributedString alloc] initWithString:@"" attributes:@{}];
     
-    for (id representedObject in self.representedObjects) {
+    for (id representedObject in representedObjects) {
         NSString *text = [representedObject description];
         
         if ([self.delegate respondsToSelector:@selector(tokenTextView:displayTextForRepresentedObject:)]) {
@@ -334,11 +357,13 @@ static void *kObservingContext = &kObservingContext;
 }
 
 - (void)_showCompletionsTableView; {
+    // if the completion range is zero length, hide the completions table view
     if ([self _completionRangeForRange:self.selectedRange].length == 0) {
         [self _hideCompletionsTableViewAndSelectCompletion:nil];
         return;
     }
     
+    // if our completion table view doesn't exist, create it and ask the delegate to display it
     if (!self.tableView) {
         if ([self.delegate respondsToSelector:@selector(tokenTextView:showCompletionsTableView:)]) {
             [self setTableView:[[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain]];
@@ -350,6 +375,7 @@ static void *kObservingContext = &kObservingContext;
         }
     }
     
+    // if the delegate responds to either of the completion returning methods, continue
     if ([self.delegate respondsToSelector:@selector(tokenTextView:completionsForSubstring:indexOfRepresentedObject:completion:)] ||
         [self.delegate respondsToSelector:@selector(tokenTextView:completionsForSubstring:indexOfRepresentedObject:)]) {
         
@@ -357,7 +383,8 @@ static void *kObservingContext = &kObservingContext;
         [self _tokenTextAttachmentForRange:self.selectedRange index:&index];
         
         NSRange range = [self _completionRangeForRange:self.selectedRange];
-        NSLog(@"%@",NSStringFromRange(range));
+        
+        // prefer the async completions delegate method
         if ([self.delegate respondsToSelector:@selector(tokenTextView:completionsForSubstring:indexOfRepresentedObject:completion:)]) {
             [self.delegate tokenTextView:self completionsForSubstring:[self.text substringWithRange:range] indexOfRepresentedObject:index completion:^(NSArray *completions) {
                 [self setCompletions:completions];
@@ -369,7 +396,9 @@ static void *kObservingContext = &kObservingContext;
     }
 }
 - (void)_hideCompletionsTableViewAndSelectCompletion:(id<BBTokenCompletion>)completion; {
+    // if the delegate responds, ask it to hide the completions table view
     if ([self.delegate respondsToSelector:@selector(tokenTextView:hideCompletionsTableView:)]) {
+        // if we were given a completion to insert, do it
         if (completion) {
             id representedObject = [self.delegate tokenTextView:self representedObjectForEditingText:[completion tokenCompletionTitle]];
             NSString *text = [self.delegate tokenTextView:self displayTextForRepresentedObject:representedObject];
@@ -466,12 +495,14 @@ static void *kObservingContext = &kObservingContext;
     [self.tableView reloadData];
 }
 - (void)setSelectedTextAttachmentRanges:(NSIndexSet *)selectedTextAttachmentRanges {
+    // force a display of the old selected token ranges
     [_selectedTextAttachmentRanges enumerateRangesUsingBlock:^(NSRange range, BOOL *stop) {
         [self.layoutManager invalidateDisplayForCharacterRange:range];
     }];
     
     _selectedTextAttachmentRanges = [selectedTextAttachmentRanges copy];
     
+    // force a display of the new selected token ranges
     [_selectedTextAttachmentRanges enumerateRangesUsingBlock:^(NSRange range, BOOL *stop) {
         [self.layoutManager invalidateDisplayForCharacterRange:range];
     }];
