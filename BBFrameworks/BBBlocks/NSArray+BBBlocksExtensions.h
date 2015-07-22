13c656e5e60b2518e6ca8703257c2d2d3ffe7d7b
William Towe <willbur1984@gmail.com>
2015-07-22 19:14:21 -0400

    added comments to NSArray+BBBlocksExtensions.h

--- BBFrameworks/BBBlocks/NSArray+BBBlocksExtensions.h
+++ BBFrameworks/BBBlocks/NSArray+BBBlocksExtensions.h
@@ -15,14 +15,92 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ Predicate block returning a boolean and taking an object and index.
+ 
+ @param object The object in the array
+ @param index The index of object in the array
+ @return YES or NO if the object passes predicate
+ */
+typedef BOOL(^BBBlockPredicateObjectAndIndexBlock)(id object, NSInteger index);
+/**
+ Block returning a new object given an object and index.
+ 
+ @param object The object in the array
+ @param index The index of object in the array
+ @return The new object
+ */
+typedef id(^BBBlockObjectAndIndexBlock)(id object, NSInteger index);
+/**
+ Block returning the new sum given object and index.
+ 
+ @param sum The current sum of the reduction
+ @param object The object in the array
+ @param index The index of object in the array
+ @return The new sum
+ */
+typedef id(^BBBlockSumObjectAndIndexBlock)(id sum, id object, NSInteger index);
+
+/**
+ Category on NSArray adding block extensions.
+ */
 @interface NSArray (BBBlocksExtensions)
 
-- (NSArray *)BB_filter:(BOOL(^)(id object, NSInteger index))block;
-- (id)BB_find:(BOOL(^)(id object, NSInteger index))block;
-- (id)BB_findWithIndex:(BOOL(^)(id object, NSInteger index))block;
-- (NSArray *)BB_map:(id(^)(id object, NSInteger index))block;
-- (id)BB_reduceWithStart:(id)start block:(id(^)(id sum, id object, NSInteger index))block;
-- (BOOL)BB_any:(BOOL(^)(id object, NSInteger index))block;
-- (BOOL)BB_all:(BOOL(^)(id object, NSInteger index))block;
+/**
+ Create and return a new array by enumerating the receiver, invoking block for each object, and including it in the new array if block returns YES.
+ 
+ @param block The block to invoke for each object in the receiver
+ @return The new array
+ @exception NSException Thrown if block is nil
+ */
+- (NSArray *)BB_filter:(BBBlockPredicateObjectAndIndexBlock)block;
+/**
+ Return the first object in the receiver for which block returns YES, or nil if block returns NO for all objects in the receiver.
+ 
+ @param block The block to invoke for each object in the receiver
+ @return The matching object or nil
+ @exception NSException Thrown if block is nil
+ */
+- (id)BB_find:(BBBlockPredicateObjectAndIndexBlock)block;
+/**
+ Return an array of the first object in the receiver along with its index for which block returns YES, or nil if block returns NO for all objects in the receiver.
+ 
+ @param block The block to invoke for each object in the receiver
+ @return An array where the first object is an object in the receiver and second object is the index of the object in the receiver, or nil
+ @exception NSException Thrown if block is nil
+ */
+- (NSArray *)BB_findWithIndex:(BBBlockPredicateObjectAndIndexBlock)block;
+/**
+ Create and return a new array by enumerating the receiver, invoking block for each object, and including the return value of block in the new array.
+ 
+ @param block The block to invoke for each object in the receiver
+ @return The new array
+ */
+- (NSArray *)BB_map:(BBBlockObjectAndIndexBlock)block;
+/**
+ Return a new object that is the result of enumerating the receiver and invoking block, passing the current sum, the object, and the index of object in the receiver. The return value of block is passed in as sum to the next invocation of block.
+ 
+ @param start The starting value for the reduction
+ @param block The block to invoke for each object in the receiver
+ @return The result of the reduction
+ @exception NSException Thrown if block is nil
+ */
+- (id)BB_reduceWithStart:(id)start block:(BBBlockSumObjectAndIndexBlock)block;
+/**
+ Return YES if block returns YES for any object in the receiver, otherwise NO.
+ 
+ @param block The block to invoke for each object in the receiver
+ @return YES if block returns YES for any object, otherwise NO
+ @exception NSException Thrown if block is nil
+ */
+- (BOOL)BB_any:(BBBlockPredicateObjectAndIndexBlock)block;
+/**
+ Return YES if block returns YES for all objects in the receiver, otherwise NO.
+ 
+ @param block The block to invoke for each object in the receiver
+ @return YES if block returns YEs for all objects, otherwise NO
+ @exception NSException Throw if block is nil
+ */
+- (BOOL)BB_all:(BBBlockPredicateObjectAndIndexBlock)block;
 
 @end
