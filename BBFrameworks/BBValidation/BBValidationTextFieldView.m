012a371f0483c09d890714ff7b16db4bcf13fc1d
William Towe <willbur1984@gmail.com>
2015-07-26 15:24:08 -0400

    start building out BBValidation

--- BBFrameworks/BBValidation/BBValidationTextFieldView.m
+++ BBFrameworks/BBValidation/BBValidationTextFieldView.m
@@ -1,8 +1,8 @@
 //
-//  BBTextFieldErrorView.m
+//  BBValidationTextFieldView.m
 //  BBFrameworks
 //
-//  Created by William Towe on 7/9/15.
+//  Created by William Towe on 7/26/15.
 //  Copyright (c) 2015 Bion Bilateral, LLC. All rights reserved.
 //
 //  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -13,16 +13,15 @@
 //
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#import "BBTextFieldErrorView.h"
-#import "UIViewController+BBKitExtensions.h"
-#import "BBTooltipViewController.h"
-#import "BBTooltipView.h"
+#import "BBValidationTextFieldView.h"
+#import "BBKit.h"
+#import "BBTooltip.h"
 
-@interface BBTextFieldErrorViewTooltipViewController : BBTooltipViewController
+@interface BBValidationTooltipViewController : BBTooltipViewController
 
 @end
 
-@implementation BBTextFieldErrorViewTooltipViewController
+@implementation BBValidationTooltipViewController
 
 - (instancetype)init {
     if (!(self = [super init]))
@@ -35,67 +34,56 @@
 
 @end
 
-@interface BBTextFieldErrorView ()
+@interface BBValidationTextFieldView ()
 @property (strong,nonatomic) UIButton *button;
 
-- (void)_BBTextFieldErrorViewInit;
+@property (strong,nonatomic) NSError *error;
 @end
 
-@implementation BBTextFieldErrorView
+@implementation BBValidationTextFieldView
 
 + (void)initialize {
-    if (self == [BBTextFieldErrorView class]) {
-        [[BBTooltipView appearanceWhenContainedIn:[BBTextFieldErrorViewTooltipViewController class], nil] setTooltipBackgroundColor:[UIColor redColor]];
-        [[BBTooltipView appearanceWhenContainedIn:[BBTextFieldErrorViewTooltipViewController class], nil] setTooltipTextColor:[UIColor whiteColor]];
+    if (self == [BBValidationTextFieldView class]) {
+        [[BBTooltipView appearanceWhenContainedIn:[BBValidationTooltipViewController class], nil] setTooltipTextColor:[UIColor whiteColor]];
+        [[BBTooltipView appearanceWhenContainedIn:[BBValidationTooltipViewController class], nil] setTooltipBackgroundColor:BBColorRGB(0.75, 0, 0)];
     }
-}
-
-- (instancetype)initWithFrame:(CGRect)frame {
-    if (!(self = [super initWithFrame:frame]))
-        return nil;
-    
-    [self _BBTextFieldErrorViewInit];
-    
-    return self;
-}
-- (id)initWithCoder:(NSCoder *)aDecoder {
-    if (!(self = [super initWithCoder:aDecoder]))
-        return nil;
-    
-    [self _BBTextFieldErrorViewInit];
-    
-    return self;
 }
 
 - (void)layoutSubviews {
     [self.button setFrame:self.bounds];
 }
+
 - (CGSize)sizeThatFits:(CGSize)size {
     return [self.button sizeThatFits:size];
 }
 
-- (void)_BBTextFieldErrorViewInit; {
+- (instancetype)initWithError:(NSError *)error; {
+    if (!(self = [super initWithFrame:CGRectZero]))
+        return nil;
+    
+    [self setError:error];
+    
     [self setButton:[UIButton buttonWithType:UIButtonTypeSystem]];
     [self.button setImage:({
-        UIImage *retval;
-        
         UIGraphicsBeginImageContextWithOptions(CGSizeMake(22, 22), NO, 0);
         
-        [[UIColor redColor] setFill];
+        [BBColorRGB(0.75, 0, 0) setFill];
         [[UIBezierPath bezierPathWithOvalInRect:CGRectMake(0, 0, 22, 22)] fill];
         
-        retval = [UIGraphicsGetImageFromCurrentImageContext() imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+        UIImage *image = [UIGraphicsGetImageFromCurrentImageContext() imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
         
         UIGraphicsEndImageContext();
         
-        retval;
+        image;
     }) forState:UIControlStateNormal];
     [self.button addTarget:self action:@selector(_buttonAction:) forControlEvents:UIControlEventTouchUpInside];
     [self addSubview:self.button];
+    
+    return self;
 }
 
 - (IBAction)_buttonAction:(id)sender {
-    [[UIViewController BB_viewControllerForPresenting] BB_presentTooltipViewControllerWithText:@"this is an error coming from the error view" attachmentView:self tooltipViewControllerClass:[BBTextFieldErrorViewTooltipViewController class]];
+    [[UIViewController BB_viewControllerForPresenting] BB_presentTooltipViewControllerWithText:self.error.BB_alertMessage attachmentView:self tooltipViewControllerClass:[BBValidationTooltipViewController class]];
 }
 
 @end
---
+++ BBFrameworks/BBValidation/UITextField+BBValidationExtensions.m
@@ -0,0 +1,88 @@
+//
+//  UITextField+BBValidationExtensions.m
+//  BBFrameworks
+//
+//  Created by William Towe on 7/26/15.
+//  Copyright (c) 2015 Bion Bilateral, LLC. All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#import "UITextField+BBValidationExtensions.h"
+#import "BBValidationTextFieldView.h"
+
+#import <objc/runtime.h>
+
+@interface _BBTextValidatorWrapper : NSObject
+@property (strong,nonatomic) id<BBTextValidator> textValidator;
+@property (weak,nonatomic) UITextField *textField;
+
+- (instancetype)initWithTextValidator:(id<BBTextValidator>)textValidator textField:(UITextField *)textField;
+@end
+
+@implementation _BBTextValidatorWrapper
+
+- (instancetype)initWithTextValidator:(id<BBTextValidator>)textValidator textField:(UITextField *)textField {
+    if (!(self = [super init]))
+        return nil;
+    
+    NSParameterAssert(textValidator);
+    NSParameterAssert(textField);
+    
+    [self setTextValidator:textValidator];
+    [self setTextField:textField];
+    
+    [self.textField addTarget:self action:@selector(_textFieldAction:) forControlEvents:UIControlEventAllEditingEvents];
+    
+    return self;
+}
+
+- (IBAction)_textFieldAction:(id)sender {
+    NSError *outError;
+    BOOL retval = [self.textValidator validateText:self.textField.text error:&outError];
+    
+    if (retval) {
+        [self.textField setRightView:nil];
+    }
+    else {
+        [self.textField setRightView:[[BBValidationTextFieldView alloc] initWithError:outError]];
+        [self.textField.rightView sizeToFit];
+        [self.textField setRightViewMode:UITextFieldViewModeAlways];
+    }
+}
+
+@end
+
+@interface UITextField (BBValidationExtensionsPrivate)
+@property (strong,nonatomic) _BBTextValidatorWrapper *BB_textValidatorWrapper;
+@end
+
+@implementation UITextField (BBValidationExtensions)
+
+- (void)BB_addTextValidator:(id<BBTextValidator>)textValidator; {
+    [self setBB_textValidatorWrapper:[[_BBTextValidatorWrapper alloc] initWithTextValidator:textValidator textField:self]];
+}
+- (void)BB_removeTextValidator; {
+    [self setBB_textValidatorWrapper:nil];
+}
+
+@end
+
+@implementation UITextField (BBValidationExtensionsPrivate)
+
+static void *kBB_textValidatorWrapperKey = &kBB_textValidatorWrapperKey;
+
+@dynamic BB_textValidatorWrapper;
+- (_BBTextValidatorWrapper *)BB_textValidatorWrapper {
+    return objc_getAssociatedObject(self, kBB_textValidatorWrapperKey);
+}
+- (void)setBB_textValidatorWrapper:(_BBTextValidatorWrapper *)BB_textValidator {
+    objc_setAssociatedObject(self, kBB_textValidatorWrapperKey, BB_textValidator, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
+
+@end
