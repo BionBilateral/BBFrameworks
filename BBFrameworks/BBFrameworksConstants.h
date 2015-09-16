138ad55113d54477f21bb3e245999f178a6504bc
William Towe <willbur1984@gmail.com>
2015-09-16 17:33:48 -0400

    bump patch version to 8

--- BBFrameworks/BBFrameworksConstants.h
+++ BBFrameworks/BBFrameworksConstants.h
@@ -20,7 +20,7 @@
 
 static NSInteger const BBFrameworksVersionMajor = 0;
 static NSInteger const BBFrameworksVersionMinor = 9;
-static NSInteger const BBFrameworksVersionPatch = 7;
+static NSInteger const BBFrameworksVersionPatch = 8;
 
 static NSString *const BBFrameworksResourcesBundleName = @"BBFrameworksResources.bundle";
 
