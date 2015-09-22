9536bca9344724747b39ff8998b5e7460d4414d5
William Towe <willbur1984@gmail.com>
2015-09-22 11:17:38 -0400

    stop BB_mediaPickerAssetCollectionViewController from returning nil every time
    fix #36

--- BBFrameworks/BBMediaPicker/BBMediaPickerAssetCollectionViewController+BBMediaPickerExtensionsPrivate.m
+++ BBFrameworks/BBMediaPicker/BBMediaPickerAssetCollectionViewController+BBMediaPickerExtensionsPrivate.m
@@ -14,15 +14,22 @@
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import "BBMediaPickerAssetCollectionViewController+BBMediaPickerExtensionsPrivate.h"
+#import "UIViewController+BBKitExtensions.h"
 
 @implementation UIViewController (BBMediaPickerExtensionsPrivate)
 
 - (BBMediaPickerAssetCollectionViewController *)BB_mediaPickerAssetCollectionViewController {
     BBMediaPickerAssetCollectionViewController *retval = nil;
     
-    for (UIViewController *viewController in self.navigationController.viewControllers) {
-        if ([viewController isKindOfClass:[BBMediaPickerAssetCollectionViewController class]]) {
-            retval = (BBMediaPickerAssetCollectionViewController *)viewController;
+    for (UIViewController *nvc in self.navigationController.viewControllers) {
+        for (UIViewController *viewController in [nvc BB_recursiveChildViewControllers]) {
+            if ([viewController isKindOfClass:[BBMediaPickerAssetCollectionViewController class]]) {
+                retval = (BBMediaPickerAssetCollectionViewController *)viewController;
+                break;
+            }
+        }
+        if (retval) {
+            break;
         }
     }
     
