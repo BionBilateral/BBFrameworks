dea4ee826d721c724706d50a5ebd74ef228eb0af
William Towe <willbur1984@gmail.com>
2015-10-06 12:05:30 -0400

    bump minor version to 3

--- BBFrameworks/BBFrameworksConstants.h
+++ BBFrameworks/BBFrameworksConstants.h
@@ -19,7 +19,7 @@
 #import <Foundation/NSString.h>
 
 static NSInteger const BBFrameworksVersionMajor = 1;
-static NSInteger const BBFrameworksVersionMinor = 2;
+static NSInteger const BBFrameworksVersionMinor = 3;
 static NSInteger const BBFrameworksVersionPatch = 0;
 
 static NSString *const BBFrameworksResourcesBundleName = @"BBFrameworksResources.bundle";
