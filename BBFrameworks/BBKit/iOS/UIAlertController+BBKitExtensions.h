228ae5eabbcefc5a952d57e26e9a01dea7c36d37
William Towe <willbur1984@gmail.com>
2015-10-07 16:52:00 -0400

    added UIAlertController presentation methods added additional UIAlertController creation methods
    fix #41

--- BBFrameworks/BBKit/iOS/UIAlertController+BBKitExtensions.h
+++ BBFrameworks/BBKit/iOS/UIAlertController+BBKitExtensions.h
@@ -14,9 +14,20 @@
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import <UIKit/UIKit.h>
-#import "NSError+BBFoundationExtensions.h"
 
 NS_ASSUME_NONNULL_BEGIN
+
+/**
+ The constant button index used to indicate the user tapped on the cancel button.
+ */
+extern NSInteger const BBKitUIAlertControllerCancelButtonIndex;
+
+/**
+ Typedef for block that is invoked after the user has tapped on one of the alert buttons and the alert has been dismissed.
+ 
+ @param buttonIndex The index of the button that was tapped
+ */
+typedef void(^BBKitUIAlertControllerCompletionBlock)(NSInteger buttonIndex);
 
 /**
  Category on UIAlertController providing convenience methods related to creation from error objects.
@@ -24,12 +35,55 @@ NS_ASSUME_NONNULL_BEGIN
 @interface UIAlertController (BBKitExtensions)
 
 /**
- Create and return a UIAlertController instance from the provided error. This will check the specific user info keys defined in NSError+BBFoundationExtensions.h for title and message.
+ Calls `[self BB_presentAlertControllerWithError:completion:]`, passing error and nil respectively.
  
- @param error The error from which to create a UIAlertController instance
- @return The UIAlertController instance
+ @param error The error from which to create and present the alert controller
  */
-+ (UIAlertController *)BB_alertControllerWithError:(NSError *)error;
++ (void)BB_presentAlertControllerWithError:(nullable NSError *)error;
+/**
+ Calls `[self BB_presentAlertControllerWithTitle:message:cancelButtonTitle:otherButtonTitles:completion:]`, passing [error BB_alertTitle], [error BB_alertMessage], nil, nil, and completion respectively.
+ 
+ @param error The error from which to create and present the alert controller
+ @param completion The completion block to invoke after the alert controller is dismissed
+ */
++ (void)BB_presentAlertControllerWithError:(nullable NSError *)error completion:(nullable BBKitUIAlertControllerCompletionBlock)completion;
+/**
+ Creates and presents an alert controller using the provided parameters. If you want to present the alert yourself, use `BB_alertControllerWithTitle:message:cancelButtonTitle:otherButtonTitles:completion:` instead.
+ 
+ @param title The title of the alert, if nil a localized default is used
+ @param message The message of the alert, if nil a localized default is used
+ @param cancelButtonTitle The cancel button title of the alert, if nil a localized default is used
+ @param otherButtonTitles The array of other button titles to add to the receiver
+ @param completion The completion block to invoke after the alert controller is dismissed
+ */
++ (void)BB_presentAlertControllerWithTitle:(nullable NSString *)title message:(nullable NSString *)message cancelButtonTitle:(nullable NSString *)cancelButtonTitle otherButtonTitles:(nullable NSArray *)otherButtonTitles completion:(nullable BBKitUIAlertControllerCompletionBlock)completion;
+
+/**
+ Calls `[self BB_alertControllerWithError:completion:]`, passing error and nil respectively.
+ 
+ @param error The error from which to create the alert controller
+ @return The alert controller
+ */
++ (UIAlertController *)BB_alertControllerWithError:(nullable NSError *)error;
+/**
+ Calls `[self BB_alertControllerWithTitle:message:cancelButtonTitle:otherButtonTitles:completion:]`, passing [error BB_alertTitle], [error BB_alertMessage], nil, nil, and completion respectively.
+ 
+ @param error The error from which to create the alert controller
+ @param completion The completion block to invoke after the alert controller is dismissed
+ @return The alert controller
+ */
++ (UIAlertController *)BB_alertControllerWithError:(nullable NSError *)error completion:(nullable BBKitUIAlertControllerCompletionBlock)completion;
+/**
+ Creates and returns a alert controller using the provided parameters.
+ 
+ @param title The title of the alert, if nil a localized default is used
+ @param message The message of the alert, if nil a localized default is used
+ @param cancelButtonTitle The cancel button title of the alert, if nil a localized default is used
+ @param otherButtonTitles The array of other button titles to add to the receiver
+ @param completion The completion block to invoke after the alert controller is dismissed
+ @return The alert controller
+ */
++ (UIAlertController *)BB_alertControllerWithTitle:(nullable NSString *)title message:(nullable NSString *)message cancelButtonTitle:(nullable NSString *)cancelButtonTitle otherButtonTitles:(nullable NSArray *)otherButtonTitles completion:(nullable BBKitUIAlertControllerCompletionBlock)completion;
 
 @end
 
--- BBFrameworks/BBKit/iOS/UIAlertController+BBKitExtensions.m
+++ BBFrameworks/BBKit/iOS/UIAlertController+BBKitExtensions.m
@@ -14,12 +14,68 @@
 //  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #import "UIAlertController+BBKitExtensions.h"
+#import "BBFoundationMacros.h"
+#import "UIViewController+BBKitExtensions.h"
+#import "NSError+BBFoundationExtensions.h"
+
+NSInteger const BBKitUIAlertControllerCancelButtonIndex = -1;
 
 @implementation UIAlertController (BBKitExtensions)
 
-+ (UIAlertController *)BB_alertControllerWithError:(NSError *)error
++ (void)BB_presentAlertControllerWithError:(nullable NSError *)error; {
+    [self BB_presentAlertControllerWithError:error completion:nil];
+}
++ (void)BB_presentAlertControllerWithError:(nullable NSError *)error completion:(nullable BBKitUIAlertControllerCompletionBlock)completion; {
+    [self BB_presentAlertControllerWithTitle:[error BB_alertTitle] message:[error BB_alertMessage] cancelButtonTitle:nil otherButtonTitles:nil completion:completion];
+}
++ (void)BB_presentAlertControllerWithTitle:(nullable NSString *)title message:(nullable NSString *)message cancelButtonTitle:(nullable NSString *)cancelButtonTitle otherButtonTitles:(nullable NSArray *)otherButtonTitles completion:(nullable BBKitUIAlertControllerCompletionBlock)completion; {
+    UIAlertController *alertController = [self BB_alertControllerWithTitle:title message:message cancelButtonTitle:cancelButtonTitle otherButtonTitles:otherButtonTitles completion:completion];
+    
+    [[UIViewController BB_viewControllerForPresenting] presentViewController:alertController animated:YES completion:nil];
+}
+
++ (UIAlertController *)BB_alertControllerWithError:(nullable NSError *)error; {
+    return [self BB_alertControllerWithError:error completion:nil];
+}
++ (UIAlertController *)BB_alertControllerWithError:(nullable NSError *)error completion:(nullable BBKitUIAlertControllerCompletionBlock)completion
 {
-    return [self.class alertControllerWithTitle:[error BB_alertTitle] message:[error BB_alertMessage] preferredStyle:UIAlertControllerStyleAlert];
+    return [self BB_alertControllerWithTitle:[error BB_alertTitle] message:[error BB_alertMessage] cancelButtonTitle:nil otherButtonTitles:nil completion:completion];
+}
++ (UIAlertController *)BB_alertControllerWithTitle:(nullable NSString *)title message:(nullable NSString *)message cancelButtonTitle:(nullable NSString *)cancelButtonTitle otherButtonTitles:(nullable NSArray *)otherButtonTitles completion:(nullable BBKitUIAlertControllerCompletionBlock)completion; {
+    if (title.length == 0) {
+        title = BBLocalizedStringErrorAlertDefaultTitle();
+    }
+    
+    if (message.length == 0) {
+        message = BBLocalizedStringErrorAlertDefaultMessage();
+    }
+    
+    if (cancelButtonTitle.length == 0) {
+        if (otherButtonTitles.count == 0) {
+            cancelButtonTitle = BBLocalizedStringAlertDefaultSingleCancelButtonTitle();
+        }
+        else {
+            cancelButtonTitle = BBLocalizedStringAlertDefaultMultipleCancelButtonTitle();
+        }
+    }
+    
+    UIAlertController *retval = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    
+    [retval addAction:[UIAlertAction actionWithTitle:cancelButtonTitle style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        if (completion) {
+            completion(BBKitUIAlertControllerCancelButtonIndex);
+        }
+    }]];
+    
+    [otherButtonTitles enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        [retval addAction:[UIAlertAction actionWithTitle:obj style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            if (completion) {
+                completion(idx);
+            }
+        }]];
+    }];
+    
+    return retval;
 }
 
 @end
