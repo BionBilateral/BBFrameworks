10edd71f5a071c467e7745b99016f22ad189eb73
William Towe <willbur1984@gmail.com>
2015-11-04 12:33:23 -0500

    added BB_none: to NSOrderedSet

--- BBFrameworks/BBBlocks/NSOrderedSet+BBBlocksExtensions.h
+++ BBFrameworks/BBBlocks/NSOrderedSet+BBBlocksExtensions.h
@@ -89,10 +89,18 @@ NS_ASSUME_NONNULL_BEGIN
  Return YES if block returns YES for all objects in the receiver, otherwise NO.
  
  @param block The block to invoke for each object in the receiver
- @return YES if block returns YEs for all objects, otherwise NO
+ @return YES if block returns YES for all objects, otherwise NO
  @exception NSException Throw if block is nil
  */
 - (BOOL)BB_all:(BOOL(^)(id object, NSInteger index))block;
+/**
+ Return YES if block returns YES for all objects in the receiver, otherwise NO.
+ 
+ @param block The block to invoke for each object in the receiver
+ @return YES if block returns YES for all objects, otherwise NO
+ @exception NSException Throw if block is nil
+ */
+- (BOOL)BB_none:(BOOL(^)(id object, NSInteger index))block;
 /**
  Returns a new ordered set created by taking the first count objects in the receiver. If count > self.count, returns self.
  
