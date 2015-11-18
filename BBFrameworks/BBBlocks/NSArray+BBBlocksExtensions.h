06def76f7eb395b8860ccfce00dbcbfaf4c2d938
William Towe <willbur1984@gmail.com>
2015-11-18 18:36:42 -0500

    fix more ObjectType errors

--- BBFrameworks/BBBlocks/NSArray+BBBlocksExtensions.h
+++ BBFrameworks/BBBlocks/NSArray+BBBlocksExtensions.h
@@ -36,7 +36,7 @@ NS_ASSUME_NONNULL_BEGIN
  @return The new array
  @exception NSException Thrown if block is nil
  */
-- (NSArray<ObjectType> *)BB_filter:(BOOL(^)(id object, NSInteger index))block;
+- (NSArray<ObjectType> *)BB_filter:(BOOL(^)(ObjectType object, NSInteger index))block;
 /**
  Create and return a new array by enumerating the receiver, invoking block for each object, and including it in the new array if block returns NO.
  
@@ -44,7 +44,7 @@ NS_ASSUME_NONNULL_BEGIN
  @return The new array
  @exception NSException Thrown if block is nil
  */
-- (NSArray<ObjectType> *)BB_reject:(BOOL(^)(id object, NSInteger index))block;
+- (NSArray<ObjectType> *)BB_reject:(BOOL(^)(ObjectType object, NSInteger index))block;
 /**
  Return the first object in the receiver for which block returns YES, or nil if block returns NO for all objects in the receiver.
  
