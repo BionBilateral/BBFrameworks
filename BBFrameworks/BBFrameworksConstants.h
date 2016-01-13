2eca59252200dadf1744e1d0bec53629e15d1625
William Towe <willbur1984@gmail.com>
2016-01-12 22:17:21 -0500

    bump patch version to 38

--- BBFrameworks/BBFrameworksConstants.h
+++ BBFrameworks/BBFrameworksConstants.h
@@ -20,7 +20,7 @@
 
 static NSInteger const BBFrameworksVersionMajor = 1;
 static NSInteger const BBFrameworksVersionMinor = 6;
-static NSInteger const BBFrameworksVersionPatch = 37;
+static NSInteger const BBFrameworksVersionPatch = 38;
 
 static NSString *const BBFrameworksResourcesBundleName = @"BBFrameworksResources.bundle";
 
