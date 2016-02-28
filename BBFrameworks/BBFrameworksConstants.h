f5210ceca4311f67b6ac2d382d8120bec186fb50
William Towe <willbur1984@gmail.com>
2016-02-27 22:38:22 -0500

    bump patch version to 71

--- BBFrameworks/BBFrameworksConstants.h
+++ BBFrameworks/BBFrameworksConstants.h
@@ -20,7 +20,7 @@
 
 static NSInteger const BBFrameworksVersionMajor = 1;
 static NSInteger const BBFrameworksVersionMinor = 6;
-static NSInteger const BBFrameworksVersionPatch = 70;
+static NSInteger const BBFrameworksVersionPatch = 71;
 
 static NSString *const BBFrameworksResourcesBundleName = @"BBFrameworksResources.bundle";
 
