f26b23b89ba7d23ae50a5c3d0248d5f9e3822d67
William Towe <willbur1984@gmail.com>
2016-03-07 03:15:01 -0500

    bump patch version to 1

--- BBFrameworks/BBFrameworksConstants.h
+++ BBFrameworks/BBFrameworksConstants.h
@@ -20,7 +20,7 @@
 
 static NSInteger const BBFrameworksVersionMajor = 1;
 static NSInteger const BBFrameworksVersionMinor = 8;
-static NSInteger const BBFrameworksVersionPatch = 0;
+static NSInteger const BBFrameworksVersionPatch = 1;
 
 static NSString *const BBFrameworksResourcesBundleName = @"BBFrameworksResources.bundle";
 
