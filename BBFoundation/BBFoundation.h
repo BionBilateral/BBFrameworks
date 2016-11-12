dae3ea6b17edc1c7ee8ab4bedc8f74cea9087293
William Towe <willbur1984@gmail.com>
2016-11-12 16:07:30 -0500

    added BBFoundation watchOS target

--- BBFoundation/BBFoundation.h
+++ BBFoundation/BBFoundation.h
@@ -5,16 +5,19 @@
 //  Created by William Towe on 11/12/16.
 //  Copyright © 2016 Bion Bilateral, LLC. All rights reserved.
 //
+//  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-//! Project version number for BBFoundation.
 FOUNDATION_EXPORT double BBFoundationVersionNumber;
 
-//! Project version string for BBFoundation.
 FOUNDATION_EXPORT const unsigned char BBFoundationVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <BBFoundation/PublicHeader.h>
 
 #import "BBFoundationDebugging.h"
 #import "BBFoundationMacros.h"
