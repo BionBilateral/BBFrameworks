d7a55fe6131361503158350e158e7479a511a7c8
William Towe <willbur1984@gmail.com>
2016-11-13 02:22:24 -0500

    bump podspec to 1.8.72

--- BBFrameworks/BBFrameworksConstants.h
+++ BBFrameworks/BBFrameworksConstants.h
@@ -20,7 +20,7 @@
 
 static NSInteger const BBFrameworksVersionMajor = 1;
 static NSInteger const BBFrameworksVersionMinor = 8;
-static NSInteger const BBFrameworksVersionPatch = 71;
+static NSInteger const BBFrameworksVersionPatch = 72;
 
 static NSString *const BBFrameworksResourcesBundleName = @"BBFrameworksResources.bundle";
 
