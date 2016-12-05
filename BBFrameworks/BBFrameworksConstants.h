2c64237dc3a66181a7cd3ff18b27db4ff92a50a5
William Towe <willbur1984@gmail.com>
2016-12-05 17:44:49 -0500

    bump version to 1.8.76

--- BBFrameworks/BBFrameworksConstants.h
+++ BBFrameworks/BBFrameworksConstants.h
@@ -20,7 +20,7 @@
 
 static NSInteger const BBFrameworksVersionMajor = 1;
 static NSInteger const BBFrameworksVersionMinor = 8;
-static NSInteger const BBFrameworksVersionPatch = 75;
+static NSInteger const BBFrameworksVersionPatch = 76;
 
 static NSString *const BBFrameworksResourcesBundleName = @"BBFrameworksResources.bundle";
 
